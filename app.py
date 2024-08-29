b13fe5b9f796f5a258e3357f235f0be7fbba0e4a
VAVAVAAA <99593810+VAVAVAAA@users.noreply.github.com>
2024-08-29 11:00:47 +0800

    Update app.py
    换源了

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ os.chdir(f"/home/xlab-app-center")
 ui = "/home/xlab-app-center"
 ui_path = os.path.join(ui, "ComfyUI")
 os.makedirs(ui_path, exist_ok=True)
-os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://hf-mirror.com/datasets/Carmeninkunming/fast-repo-kaggle/resolve/main/cui-openxlab.tar.lz4 -o cui.tar.lz4 && tar -xI lz4 -f cui.tar.lz4 --directory={ui_path} && rm {ui}/cui.tar.lz4")
+os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M https://hf-mirror.com/datasets/Carmeninkunming/fast-repo-kaggle/resolve/main/cui-2024-8.tar.lz4?download=true -o cui.tar.lz4 && tar -xI lz4 -f cui.tar.lz4 --directory={ui_path} && rm {ui}/cui.tar.lz4")
 os.chdir(f"/home/xlab-app-center/ComfyUI")
 os.system(f"git lfs install")
 os.system(f"git reset --hard")
