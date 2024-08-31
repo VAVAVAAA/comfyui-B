9b0e91156ecd4360530dd416fe5199feb870ebb7
VAVAVAAA <99593810+VAVAVAAA@users.noreply.github.com>
2024-08-31 09:00:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,30 +1,13 @@
 import os
-
-
-# 定义根目录路径
-root_dir = "/home/xlab-app-center"
-os.chdir(root_dir)
-
-# 定义 ComfyUI 的安装路径
-ui_path = os.path.join(root_dir, "ComfyUI")
-os.makedirs(ui_path, exist_ok=True)  # 如果路径不存在，则创建
-
-# 使用 git clone 下载 ComfyUI 仓库到 ui_path
-os.system(f"git clone https://github.com/comfyanonymous/ComfyUI.git {ui_path}")
-
-# 解压缩函数，由于不再使用 7z 文件下载，这部分代码可以省略
-# archive_path = os.path.join(root_dir, "comfyUI.7z")
-# with py7zr.SevenZipFile(archive_path, mode='r') as z:  # 使用 py7zr 打开下载的 7z 文件
-#     z.extractall(path=ui_path)  # 解压所有文件到目标目录 ui_path 中
-# os.remove(archive_path)  # 删除下载的 7z 文件，以节省磁盘空间
-
-# 切换到 ComfyUI 目录
-os.chdir(ui_path)
-
-# 安装 Git LFS（大文件存储）并重置 Git 仓库状态
-os.system("git lfs install")
-os.system("git reset --hard")
-
-# 运行 ComfyUI 主程序
-os.system("python main.py --dont-print-server --preview-method auto --enable-cors-header --use-pytorch-cross-attention")
+import shutil
+os.chdir(f"/home/xlab-app-center")
+os.system(f"git clone -b https://github.com/comfyanonymous/ComfyUI /home/xlab-app-center")
+os.chdir(f"/home/xlab-app-center")
+os.system(f"git lfs install")
+os.system(f"git reset --hard")
+os.system(f"git clone https://github.com/ltdrdata/ComfyUI-Manager /home/xlab-app-center/custom_nodes/ComfyUI-Manager")
+os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M --async-dns=false https://huggingface.co/Comfy-Org/flux1-dev/resolve/main/flux1-dev-fp8.safetensors?download=true -d /home/xlab-app-center/models/checkpoints -o flux1-dev-fp8.safetensors")
+os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M --async-dns=false https://civitai.com/api/download/models/782002?type=Model&format=SafeTensor&size=full&fp=fp16 -d /home/xlab-app-center/models/checkpoints -o Jugg_Xl_by_RunDiffusion.safetensors")
+os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M --async-dns=false https://download.openxlab.org.cn/models/ninjawick/realistic-vision-5.1/weight//Realistic_Vision_V6.0_NV_B1_inpainting.safetensors -d /home/xlab-app-center/models/checkpoints -o Realistic_Vision_V6.0_NV_B1_inpainting.safetensors")
+os.system(f"python main.py --listen 0.0.0.0 --port 7860 --enable-cors-header")
 
