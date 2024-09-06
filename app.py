a7c22e8593592f2afb4de9e1adc6b523d8bad204
VAVAVAAA <99593810+VAVAVAAA@users.noreply.github.com>
2024-09-06 08:07:26 +0800

    Update app.py

--- app.py
+++ app.py
@@ -44,6 +44,8 @@ os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M --async-dns=fa
 os.system(f"aria2c --console-log-level=error -c -x 16 -s 16 -k 1M --async-dns=false https://hf-mirror.com/Shakker-Labs/FLUX.1-dev-ControlNet-Union-Pro/resolve/main/diffusion_pytorch_model.safetensors?download=true -d ComfyUI/home/xlab-app-center/models/controlnet -o FLUX.1-dev-ControlNet-Union-Pro.safetensors")
 # 反推模型
 os.system("huggingface-cli download --resume-download google/siglip-so400m-patch14-384 --local-dir /home/xlab-app-center/models/clip/siglip-so400m-patch14-384")
-
-os.system(f"python ComfyUI/main.py --listen 0.0.0.0 --port 7860 --enable-cors-header")
+os.system("ls /home/xlab-app-center/ComfyUI")
+#os.system("ls /home/xlab-app-center/")
+os.chdir(f"/home/xlab-app-center/ComfyUI")
+os.system(f"python main.py --listen 0.0.0.0 --port 7860 --enable-cors-header")
 
