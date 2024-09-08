d0909ec660b630c2e9f110907b7afc5c31839ce2
VAVAVAAA <99593810+VAVAVAAA@users.noreply.github.com>
2024-09-08 09:45:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ os.system(f"git clone https://git.homegu.com/comfyanonymous/ComfyUI")
 os.system(f"git lfs install")
 # os.system(f"git reset --hard")
 os.system("pip install --upgrade transformers==4.44.2")
-os.system("pip install --upgrade torch==2.1.0 torchvision==0.16.0")
-os.environ["HF_ENDPOINT"] = "https://hf-mirror.com"
-os.system("pip install --upgrade huggingface_hub")
+os.system("pip install --upgrade torch==2.1.2 torchvision==0.16.0")
+
 
 os.system(f"git clone https://git.homegu.com/ltdrdata/ComfyUI-Manager.git ComfyUI/home/xlab-app-center/custom_nodes/ComfyUI-Manager")
 os.system(f"git clone https://git.homegu.com/ty0x2333/ComfyUI-Dev-Utils ComfyUI/home/xlab-app-center/custom_nodes/ComfyUI-Dev-Utils") # 显示节点运行时间
